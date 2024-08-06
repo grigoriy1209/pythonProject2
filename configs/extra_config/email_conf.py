d131018617baf2f7b394e2da51c719c3778856c8
Grigoriy1209 <grigoriyv1209@gmail.com>
2024-08-06 15:44:54 +0300

    lesson7

---
+++ configs/extra_config/email_conf.py
@@ -0,0 +1,7 @@
+import os
+
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_USER = os.environ.get("EMAIL_USER")
+EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_USE_TLS = True
